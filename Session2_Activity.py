90d970cd7a1fdb00da3c4e5bac8d78c3c18a9e0c
Ochirrej <monmon032313@gmail.com>
2024-09-08 14:19:32 +0800

    #2_done

--- Session2_Activity.py
+++ Session2_Activity.py
@@ -4,6 +4,13 @@ b = 12
 
 print(type(a))
 print(type(b))
+#
 
-#================
+
+#======= 2 =======
+print(f'a + b = {a + b}') # addition
+print(f'a - b = {a - b}') # subtraction
+print(f'a * b = {a * b}') # multiplication
+print(f'a / b = {a / b}') # division
+#
 
