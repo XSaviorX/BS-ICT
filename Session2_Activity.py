792c3ebef15ed67b53ef2dfafb6383768667504e
Ochirrej <monmon032313@gmail.com>
2024-09-08 15:04:21 +0800

    last

--- Session2_Activity.py
+++ Session2_Activity.py
@@ -14,3 +14,24 @@ print(f'a * b = {a * b}') # multiplication
 print(f'a / b = {a / b}') # division
 #
 
+#======= 3 =======
+c = int(a / b)
+print(f'Value of c = {c} \nType of c  = {type(c)}')
+
+c = float(a/b)
+print(f'Value of c = {c} \nType of c  = {type(c)}')
+
+#
+
+#======= 4 ======
+message = "The result of a divided by b is"
+c_message = message + str(c)
+print(c_message)
+# 
+
+#====== 5 =======
+res1 = a > b
+res2 = a == b
+print(res1)
+print(res2)
+#
